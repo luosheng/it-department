6f491057d099116c735f52c01702e05e34fc429a
Sheng Luo <47009+luosheng@users.noreply.github.com>
2023-11-16 17:36:18 +0800

    Initial commit

---
+++ ItDepartment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ItDepartment
+//
+//  Created by Luo Sheng on 2023/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ItDepartment/ItDepartmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItDepartmentApp.swift
+//  ItDepartment
+//
+//  Created by Luo Sheng on 2023/11/16.
+//
+
+import SwiftUI
+
+@main
+struct ItDepartmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
