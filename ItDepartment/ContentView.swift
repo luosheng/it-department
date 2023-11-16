de4e615364322ba1f151dfd2991c45e8bbad0a73
Sheng Luo <47009+luosheng@users.noreply.github.com>
2023-11-16 20:25:38 +0800

    Write apple script to script extension path

--- ItDepartment/ContentView.swift
+++ ItDepartment/ContentView.swift
@@ -16,6 +16,35 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .task {
+            setupScript()
+        }
+    }
+    
+    private func setupScript() {
+        guard let scriptPath = try? FileManager.default.url(for: .applicationScriptsDirectory, in: .userDomainMask, appropriateFor: nil, create: true) else {
+            return
+        }
+        let killScriptPath = scriptPath
+            .deletingLastPathComponent()
+            .appending(path: "com.pop-tap.ItDepartment.FinderExtension")
+            .appending(path: "kill-scim.scpt")
+        print(killScriptPath.path(percentEncoded: false))
+        guard !FileManager.default.fileExists(atPath: killScriptPath.path(percentEncoded: false)) else {
+            return
+        }
+        let killScript = """
+tell application "Finder"
+    activate
+    do shell script "pkill -9 SCIM"
+    do shell script "pkill -9 SCIM_Extension"
+end tell
+"""
+        do {
+            try killScript.write(to: killScriptPath, atomically: true, encoding: .utf8)
+        } catch {
+            print("Error writing string to file: \(error)")
+        }
     }
 }
 
